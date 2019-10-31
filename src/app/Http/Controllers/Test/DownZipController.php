68cd4483b42f6d228c109481dfbdacf062c3e41a
zdcrobot <1474083407@qq.com>
2019-10-31 21:53:15 +0800

    route start

---
+++ src/app/Http/Controllers/Test/DownZipController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http\Controllers\Test;
+use App\Http\Controllers\Controller;
+
+class DownZipController extends Controller
+{
+    public function index(){
+        echo "999";
+    }
+}
